d47cedfda1835cf1debbf6d5afa3461813f3f8a5
Felipe Kuhne <felipe.kuhne@hp.com>
2023-04-06 23:46:07 -0300

    wip

--- courses/matching-game/src/components/CardFrontBack/index.js
+++ courses/matching-game/src/components/CardFrontBack/index.js
@@ -2,8 +2,12 @@ import "./style.css";
 import CardGame from "../CardGame";
 
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        event.target.closest('.card-front-back').classList.toggle("-active");
+    }
     return /*html*/ `
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
