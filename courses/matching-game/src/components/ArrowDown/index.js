5e903c494e34013dc11b8c77918c04b559e7690e
Felipe Kuhne <felipe.kuhne@hp.com>
2023-04-16 18:55:57 -0300

    wip

---
+++ courses/matching-game/src/components/ArrowDown/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import arrowDownSvg from "../../../images/IconArrowDown.svg";
+
+function ArrowDown() {
+    return /*html*/ `
+        <img src=${arrowDownSvg} alt="arrow down" />
+    `;
+}
+
+export default ArrowDown;
