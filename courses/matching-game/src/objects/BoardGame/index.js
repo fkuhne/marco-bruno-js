c01e7c9e0c023fdaf27a11048c79feb86de3ba0d
Felipe Kuhne <felipe.kuhne@hp.com>
2023-04-16 22:15:11 -0300

    wip

--- courses/matching-game/src/objects/BoardGame/index.js
+++ courses/matching-game/src/objects/BoardGame/index.js
@@ -3,12 +3,35 @@ import CardFrontBack from "../../components/CardFrontBack";
 import { cards } from "./data";
 
 function BoardGame(amountCards) {
+  const flipAndHideCards = active_cards => {
+    active_cards.forEach(card => card.classList.remove("-active"));
+  }
+
+  const togglePlayer = () => {
+    const $arrowDown = document.querySelector(".arrow-down")
+    const currentPlayer = $arrowDown.getAttribute("data-currentPlayer")
+    $arrowDown.setAttribute("data-currentPlayer", currentPlayer === "1" ? "2" : "1")  
+  }
+
+  window.boardGame = {};
+  window.boardGame.handleClick = () => {
+    
+    const $boardGame = document.querySelector(".board-game")
+    const $activeCards = $boardGame.querySelectorAll(".card-front-back.-active") 
+
+    if($activeCards.length === 2) {
+      setTimeout(() => {
+        flipAndHideCards($activeCards)
+        togglePlayer()
+      }, "900");
+    }
+  }
 
   let $htmlCardsList = cards.map(card => CardFrontBack(card.icon, card.altIcon))
   $htmlCardsList = $htmlCardsList.join('')
 
   return /*html*/ `
-    <section class="board-game">
+    <section class="board-game" onClick="boardGame.handleClick()">
       ${$htmlCardsList}
     </section>
   `;
